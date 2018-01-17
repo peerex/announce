0236a54c283c410cf17e5be63c0b942c7f08c388
Mr. Peerex <mr@peerex.org>
2018-01-17 05:49:45 +0000

    New phaze

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,3 +4,5 @@ document.addEventListener('DOMContentLoaded', function () {
   /* eslint-disable no-new */
   new SmoothScroll('a[href*="#"]')
 })
+
+console.log(btoa('Hello, you hacker! Send your CV to mail@peerex.org'))
